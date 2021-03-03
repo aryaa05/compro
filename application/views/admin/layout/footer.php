425e7d6d12adb276a42eb3aa7f88fb2582162edf
agusarya05 <agus.arya@podomorocitydelimedan.com>
2021-03-03 12:47:11 +0700

    Update isi Konten

--- application/views/admin/layout/footer.php
+++ application/views/admin/layout/footer.php
@@ -1,165 +1,162 @@
 <script>
-tinymce.init({
-  selector: '#isi',
-  height: 300,
-  plugins: 'print preview paste searchreplace autolink directionality visualblocks visualchars code fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount imagetools colorpicker textpattern help',
-  toolbar: 'formatselect | fontsizeselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent | image | table | removeformat',
-  visual_table_class: 'tiny-table',
-   fontsize_formats: "8px 10px 12px 14px 18px 24px 36px"
- });
-
+  tinymce.init({
+    selector: '#isi',
+    height: 300,
+    plugins: 'print preview paste searchreplace autolink directionality visualblocks visualchars code fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount imagetools colorpicker textpattern help',
+    toolbar: 'formatselect | fontsizeselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent | image | table | removeformat',
+    visual_table_class: 'tiny-table',
+    fontsize_formats: "8px 10px 12px 14px 18px 24px 36px"
+  });
 </script>
 
 <script>
-tinymce.init({
-  selector: '.textarea',
-  height: 300,
-  plugins: 'print preview paste searchreplace autolink directionality visualblocks visualchars code fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount imagetools colorpicker textpattern help',
-  toolbar: 'formatselect | fontsizeselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent | image | table | removeformat',
-  visual_table_class: 'tiny-table',
-   fontsize_formats: "8px 10px 12px 14px 18px 24px 36px"
- });
-
+  tinymce.init({
+    selector: '.textarea',
+    height: 300,
+    plugins: 'print preview paste searchreplace autolink directionality visualblocks visualchars code fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount imagetools colorpicker textpattern help',
+    toolbar: 'formatselect | fontsizeselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent | image | table | removeformat',
+    visual_table_class: 'tiny-table',
+    fontsize_formats: "8px 10px 12px 14px 18px 24px 36px"
+  });
 </script>
 <script>
-tinymce.init({
-  selector: '.textareatengah',
-  height: 500,
-  plugins: 'print preview paste searchreplace autolink directionality visualblocks visualchars code fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount imagetools colorpicker textpattern help',
-  toolbar: 'formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent | image | table | removeformat',
-  visual_table_class: 'tiny-table'
- });
-
+  tinymce.init({
+    selector: '.textareatengah',
+    height: 500,
+    plugins: 'print preview paste searchreplace autolink directionality visualblocks visualchars code fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount imagetools colorpicker textpattern help',
+    toolbar: 'formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent | image | table | removeformat',
+    visual_table_class: 'tiny-table'
+  });
 </script>
 <!-- SWEETALERT -->
-<?php if($this->session->flashdata('sukses')) { ?>
-<script>
-  swal("Berhasil", "<?php echo $this->session->flashdata('sukses'); ?>","success")
-</script>
+<?php if ($this->session->flashdata('sukses')) { ?>
+  <script>
+    swal("Berhasil", "<?php echo $this->session->flashdata('sukses'); ?>", "success")
+  </script>
 <?php } ?>
 
-<?php if(isset($error)) { ?>
-<script>
-  swal("Oops...", "<?php echo strip_tags($error); ?>","warning")
-</script>
+<?php if (isset($error)) { ?>
+  <script>
+    swal("Oops...", "<?php echo strip_tags($error); ?>", "warning")
+  </script>
 <?php } ?>
 
-<?php if($this->session->flashdata('warning')) { ?>
-<script>
-  swal("Oops...", "<?php echo $this->session->flashdata('warning'); ?>","warning")
-</script>
+<?php if ($this->session->flashdata('warning')) { ?>
+  <script>
+    swal("Oops...", "<?php echo $this->session->flashdata('warning'); ?>", "warning")
+  </script>
 <?php } ?>
-<?php 
+<?php
 $sek  = date('Y');
-$awal = $sek-100;
+$awal = $sek - 100;
 ?>
 <script>
-$( ".datepicker" ).datepicker({
-  inline: true,
-  changeYear: true,
-  changeMonth: true,
-  dateFormat: "yy-mm-dd",
-  yearRange: "<?php echo $awal ?>:<?php echo date('Y') ?>"
-});
+  $(".datepicker").datepicker({
+    inline: true,
+    changeYear: true,
+    changeMonth: true,
+    dateFormat: "yy-mm-dd",
+    yearRange: "<?php echo $awal ?>:<?php echo date('Y') ?>"
+  });
 
-$( ".tanggal" ).datepicker({
-  inline: true,
-  changeYear: true,
-  changeMonth: true,
-  dateFormat: "dd-mm-yy",
-  yearRange: "<?php echo $awal ?>:<?php echo date('Y') ?>"
-});
+  $(".tanggal").datepicker({
+    inline: true,
+    changeYear: true,
+    changeMonth: true,
+    dateFormat: "dd-mm-yy",
+    yearRange: "<?php echo $awal ?>:<?php echo date('Y') ?>"
+  });
 </script>
 <script>
-// Sweet alert
-function confirmation(ev) {
-ev.preventDefault();
-var urlToRedirect = ev.currentTarget.getAttribute('href'); //use currentTarget because the click may be on the nested i tag and not a tag causing the href to be empty
-console.log(urlToRedirect); // verify if this is the right URL
-swal({
-  title: "Yakin ingin menghapus data ini?",
-  text: "Data yang sudah dihapus tidak dapat dikembalikan",
-  icon: "warning",
-  buttons: true,
-  dangerMode: true,
-})
-.then((willDelete) => {
-  // redirect with javascript here as per your logic after showing the alert using the urlToRedirect value
-  if (willDelete) {
-    // Proses ke URL
-    window.location.href = urlToRedirect;
-  } 
-});
-} 
+  // Sweet alert
+  function confirmation(ev) {
+    ev.preventDefault();
+    var urlToRedirect = ev.currentTarget.getAttribute('href'); //use currentTarget because the click may be on the nested i tag and not a tag causing the href to be empty
+    console.log(urlToRedirect); // verify if this is the right URL
+    swal({
+        title: "Yakin ingin menghapus data ini?",
+        text: "Data yang sudah dihapus tidak dapat dikembalikan",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        // redirect with javascript here as per your logic after showing the alert using the urlToRedirect value
+        if (willDelete) {
+          // Proses ke URL
+          window.location.href = urlToRedirect;
+        }
+      });
+  }
 
-// Kirim ulang
-function kirim(ev) {
-ev.preventDefault();
-var urlToRedirect = ev.currentTarget.getAttribute('href'); //use currentTarget because the click may be on the nested i tag and not a tag causing the href to be empty
-console.log(urlToRedirect); // verify if this is the right URL
-swal({
-  title: "Yakin Ingin Mengirim Surat Ini?",
-  text: "Pengiriman Surat Sebaiknya Kurang dari 200 Kali/jam agar tidak terkena Blokir Server. Klik CANCEL untuk membatalkan. Klik OK untuk mengirim surat.",
-  icon: "warning",
-  buttons: true,
-  dangerMode: true,
-})
-.then((willDelete) => {
-  // redirect with javascript here as per your logic after showing the alert using the urlToRedirect value
-  if (willDelete) {
-    // Proses ke URL
-    window.location.href = urlToRedirect;
-  } 
-});
-} 
-// Akses
-// Sweet alert
-function akses(ev) {
-ev.preventDefault();
-var urlToRedirect = ev.currentTarget.getAttribute('href'); //use currentTarget because the click may be on the nested i tag and not a tag causing the href to be empty
-console.log(urlToRedirect); // verify if this is the right URL
-swal({
-  title: "Yakin ingin memberi akses?",
-  text: "Data yang diberi akses akan bisa login",
-  icon: "warning",
-  buttons: true,
-  dangerMode: true,
-})
-.then((willDelete) => {
-  // redirect with javascript here as per your logic after showing the alert using the urlToRedirect value
-  if (willDelete) {
-    // Proses ke URL
-    window.location.href = urlToRedirect;
-  } 
-});
-} 
+  // Kirim ulang
+  function kirim(ev) {
+    ev.preventDefault();
+    var urlToRedirect = ev.currentTarget.getAttribute('href'); //use currentTarget because the click may be on the nested i tag and not a tag causing the href to be empty
+    console.log(urlToRedirect); // verify if this is the right URL
+    swal({
+        title: "Yakin Ingin Mengirim Surat Ini?",
+        text: "Pengiriman Surat Sebaiknya Kurang dari 200 Kali/jam agar tidak terkena Blokir Server. Klik CANCEL untuk membatalkan. Klik OK untuk mengirim surat.",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        // redirect with javascript here as per your logic after showing the alert using the urlToRedirect value
+        if (willDelete) {
+          // Proses ke URL
+          window.location.href = urlToRedirect;
+        }
+      });
+  }
+  // Akses
+  // Sweet alert
+  function akses(ev) {
+    ev.preventDefault();
+    var urlToRedirect = ev.currentTarget.getAttribute('href'); //use currentTarget because the click may be on the nested i tag and not a tag causing the href to be empty
+    console.log(urlToRedirect); // verify if this is the right URL
+    swal({
+        title: "Yakin ingin memberi akses?",
+        text: "Data yang diberi akses akan bisa login",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        // redirect with javascript here as per your logic after showing the alert using the urlToRedirect value
+        if (willDelete) {
+          // Proses ke URL
+          window.location.href = urlToRedirect;
+        }
+      });
+  }
 </script>
 
 </div>
-            <!-- /.card-body -->
-          </div>
-          <!-- /.card -->
-        </div>
-        <!-- /.col -->
-      </div>
-      <!-- /.row -->
-    </section>
-    <!-- /.content -->
+<!-- /.card-body -->
+</div>
+<!-- /.card -->
+</div>
+<!-- /.col -->
+</div>
+<!-- /.row -->
+</section>
+<!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+<footer class="main-footer">
+  <div class="float-right d-none d-sm-block">
+    <b>Version</b> 3.0.0-alpha
   </div>
-  <!-- /.content-wrapper -->
-  <footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-      <b>Version</b> 3.0.0-alpha
-    </div>
-    <strong>Copyright &copy; 2014-2018 <a href="http://adminlte.io">AdminLTE.io</a>.</strong> All rights
-    reserved.
-  </footer>
+  <strong>Copyright &copy; <?php echo date('Y') ?>.</a> <a href="http://adminlte.io">AdminLTE.io</a>.</strong> All rights
+  reserved.
+</footer>
 
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+  <!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-sidebar -->
 </div>
 <!-- ./wrapper -->
 <!-- Bootstrap 4 -->
@@ -185,7 +182,7 @@ swal({
 <script src="<?php echo base_url() ?>assets/admin/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
 <!-- page script -->
 <script>
-  $(function () {
+  $(function() {
     $("#example1").DataTable();
     $('#example2').DataTable({
       "paging": true,
@@ -200,18 +197,18 @@ swal({
 
 <!-- Page Script -->
 <script>
-  $(function () {
-     //Initialize Select2 Elements
+  $(function() {
+    //Initialize Select2 Elements
     $('.select2').select2()
     //Enable iCheck plugin for checkboxes
     //iCheck for checkbox and radio inputs
     $('.mailbox-messages input[type="checkbox"]').iCheck({
       checkboxClass: 'icheckbox_flat-blue',
-      radioClass   : 'iradio_flat-blue'
+      radioClass: 'iradio_flat-blue'
     })
 
     //Enable check and uncheck all functionality
-    $('.checkbox-toggle').click(function () {
+    $('.checkbox-toggle').click(function() {
       var clicks = $(this).data('clicks')
       if (clicks) {
         //Uncheck all checkboxes
@@ -226,12 +223,12 @@ swal({
     })
 
     //Handle starring for glyphicon and font awesome
-    $('.mailbox-star').click(function (e) {
+    $('.mailbox-star').click(function(e) {
       e.preventDefault()
       //detect type
       var $this = $(this).find('a > i')
       var glyph = $this.hasClass('glyphicon')
-      var fa    = $this.hasClass('fa')
+      var fa = $this.hasClass('fa')
 
       //Switch states
       if (glyph) {
@@ -248,24 +245,27 @@ swal({
 </script>
 
 <script>
-  $(function () {
+  $(function() {
     // Replace the <textarea id="editor1"> with a CKEditor
     // instance, using default configuration.
     ClassicEditor
       .create(document.querySelector('#editor1'))
-      .then(function (editor) {
+      .then(function(editor) {
         // The editor instance
       })
-      .catch(function (error) {
+      .catch(function(error) {
         console.error(error)
       })
 
     // bootstrap WYSIHTML5 - text editor
 
     $('.textareaku').wysihtml5({
-      toolbar: { fa: true }
+      toolbar: {
+        fa: true
+      }
     })
   })
 </script>
 </body>
+
 </html>
